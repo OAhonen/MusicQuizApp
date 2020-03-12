69a3da2a6bfd344077f5117658adcab86e6343f9
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-12 13:54:39 +0200

    Add ability to fetch access token.

--- app/src/main/java/fi/tuni/musicquizapp/ConnectTask.java
+++ app/src/main/java/fi/tuni/musicquizapp/ConnectTask.java
@@ -1,14 +1,21 @@
 package fi.tuni.musicquizapp;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ConnectTask extends AsyncTask {
     // basicAuth = encoded Spotify client_id + client_secret
@@ -32,6 +39,32 @@ public class ConnectTask extends AsyncTask {
                 .method("POST", reqbody)
                 .url(url)
                 .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage();
+                Log.w("No response", mMessage);
+                // call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String mMessage = response.body().string();
+                try {
+                    token = new JSONObject(mMessage);
+                    Log.d("ConnectTask", token.getString("access_token"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         return null;
     }
