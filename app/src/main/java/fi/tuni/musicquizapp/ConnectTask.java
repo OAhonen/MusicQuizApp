f9fd6e5ac68d699570d64673d5ddf631608ae3e7
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-12 13:41:05 +0200

    Add required variables to connect Spotify API.

--- app/src/main/java/fi/tuni/musicquizapp/ConnectTask.java
+++ app/src/main/java/fi/tuni/musicquizapp/ConnectTask.java
@@ -1,4 +1,38 @@
 package fi.tuni.musicquizapp;
 
-public class ConnectTask {
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class ConnectTask extends AsyncTask {
+    // basicAuth = encoded Spotify client_id + client_secret
+    private String basicAuth = "Basic " + "M2UwMzkwMWQ3ZGRlNDZkMmE2MDM2M2MxZWJlOTUwN2Y6NzgyY2Y5ZDQyMjVmNDJkNjhkNjg3NWVlZDE1MDYzNDM=";
+    private JSONObject token;
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://accounts.spotify.com/api/token").newBuilder();
+        String url = urlBuilder.build().toString();
+
+        // Body of the POST-request contains "grant_type", which is set to "client_credentials"
+        // See: Client Credentials flow
+        // https://developer.spotify.com/documentation/general/guides/authorization-guide/
+        RequestBody reqbody = new FormBody.Builder().addEncoded("grant_type", "client_credentials").build();
+
+        Request request = new Request.Builder()
+                .header("Authorization", basicAuth)
+                .method("POST", reqbody)
+                .url(url)
+                .build();
+
+        return null;
+    }
 }
