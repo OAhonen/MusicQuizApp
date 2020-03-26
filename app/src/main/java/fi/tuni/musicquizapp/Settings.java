4962e48cee8dcc59df08e0c99b05c7f0c236160b
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-26 10:54:18 +0200

    Add three different countries to choose from.

--- app/src/main/java/fi/tuni/musicquizapp/Settings.java
+++ app/src/main/java/fi/tuni/musicquizapp/Settings.java
@@ -1,7 +1,10 @@
 package fi.tuni.musicquizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,15 +17,60 @@ public class Settings extends AppCompatActivity {
     private Spinner countrySpinner;
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> countriesList;
+    private final String FINLAND = "https://api.spotify.com/v1/playlists/37i9dQZEVXbMxcczTSoGwZ";
+    private final String USA = "https://api.spotify.com/v1/playlists/37i9dQZEVXbLRQDuF5jeBp";
+    private final String SWEDEN = "https://api.spotify.com/v1/playlists/37i9dQZEVXbLoATJ81JYXz";
+    private String selectedCountry;
+    private String accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            accessToken = extras.getString("accessToken");
+        }
         countriesList = new ArrayList<>(Arrays.asList("Finland", "Sweden", "USA"));
-        countrySpinner = findViewById(R.id.changeCountryID);
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, countriesList);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countrySpinner = findViewById(R.id.changeCountryID);
         countrySpinner.setAdapter(arrayAdapter);
+        selectedCountry = (String) countrySpinner.getSelectedItem();
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCountry = countriesList.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private String checkSelectedCountry(String country) {
+        String result = "";
+        switch (country) {
+            case "Finland":
+                result = FINLAND;
+                break;
+            case "USA":
+                result = USA;
+                break;
+            case "Sweden":
+                result = SWEDEN;
+                break;
+        }
+        return result;
+    }
+
+    public void backToMenuClick(View v) {
+        String result = checkSelectedCountry(selectedCountry);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("countryCode", result);
+        intent.putExtra("accessToken", accessToken);
+        startActivity(intent);
     }
 }
