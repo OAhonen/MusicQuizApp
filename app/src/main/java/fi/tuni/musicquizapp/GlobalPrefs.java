f0581857fc47692e2dedf468378da96709335e5e
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-28 09:05:58 +0200

    Add default country and countrycode to prefs.

--- app/src/main/java/fi/tuni/musicquizapp/GlobalPrefs.java
+++ app/src/main/java/fi/tuni/musicquizapp/GlobalPrefs.java
@@ -9,6 +9,10 @@ public abstract class GlobalPrefs {
     private static final String keyCountry = "country";
     private static final String keyCountryCode = "countryCode";
     private static final String keyAccessToken = "accessToken";
+    private static final String keyCountryNumber = "countryNumber";
+
+    // Default countryCode = Finnish playlist
+    private static String defCountryCode = "https://api.spotify.com/v1/playlists/37i9dQZEVXbMxcczTSoGwZ";
 
     public static void init(Context context) {
         prefs = context.getSharedPreferences("MusicQuizPrefs", Context.MODE_PRIVATE);
@@ -22,5 +26,25 @@ public abstract class GlobalPrefs {
 
     public static String getCountry() {
         return prefs.getString(keyCountry, null);
+    }
+
+    public static void setCountryCode(String countryCode) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(keyCountryCode, countryCode);
+        editor.commit();
+    }
+
+    public static String getCountryCode() {
+        return prefs.getString(keyCountryCode, defCountryCode);
+    }
+
+    public static void setCountryNumber(int countryNumber) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(keyCountryNumber, countryNumber);
+        editor.commit();
+    }
+
+    public static int getCountryNumber() {
+        return prefs.getInt(keyCountryNumber, 0);
     }
 }
