ad5c20482671bda74efd15f977647f2bddfceceb
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-30 10:42:44 +0300

    Add ability to save score.

--- app/src/main/java/fi/tuni/musicquizapp/GameOverActivity.java
+++ app/src/main/java/fi/tuni/musicquizapp/GameOverActivity.java
@@ -5,23 +5,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 import fi.tuni.musicquizapp.preferences.GlobalPrefs;
+import fi.tuni.musicquizapp.preferences.HighscorePrefs;
 
 /**
  * Game over activity.
  */
 public class GameOverActivity extends AppCompatActivity {
     private boolean[] userAnswers;
-    private TextView textView;
+    private TextView textViewResult;
     private Button mainMenu;
     private Button playAgain;
     private ArrayList<ArtistTrackPair> top10Songs;
     private String accessToken;
+    private int correctAnswers = 0;
+    private TextView textViewMadeIt;
+    private EditText editTextName;
+    private Button saveScore;
+    private int scorePosition = 0;
+    private boolean highEnough = false;
 
     /**
      * Get top-10 tracks and user's answers.
@@ -31,30 +39,80 @@ public class GameOverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameover);
-        Log.d("GOVERTOKEN", GlobalPrefs.getAccessToken());
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             userAnswers = extras.getBooleanArray("userAnswers");
             top10Songs = (ArrayList<ArtistTrackPair>) extras.getSerializable("top10");
         }
-        textView = findViewById(R.id.user_result);
+        textViewResult = findViewById(R.id.userResult);
         mainMenu = findViewById(R.id.toMenuID);
         playAgain = findViewById(R.id.playAgainID);
+        textViewMadeIt = findViewById(R.id.madeItToScores);
+        editTextName = findViewById(R.id.editName);
+        saveScore = findViewById(R.id.saveScore);
         calcResult();
+        checkHighScore();
     }
 
     /**
      * Check user's answer percentage.
      */
     private void calcResult() {
-        int correctAnswers = 0;
         for (boolean userAnswer : userAnswers) {
             if (userAnswer) {
                 correctAnswers++;
             }
         }
         double percentage = (double) correctAnswers * 10;
-        textView.setText("Nice. You got " + percentage + "% correct answers.");
+        textViewResult.setText("Nice. You got " + percentage + "% correct answers.");
+    }
+
+    private void checkHighScore() {
+        if (correctAnswers >= HighscorePrefs.getScore1()) {
+            scorePosition = 1;
+            highEnough = true;
+            HighscorePrefs.setName3(HighscorePrefs.getName2());
+            HighscorePrefs.setCountry3(HighscorePrefs.getCountry2());
+            HighscorePrefs.setScore3(HighscorePrefs.getScore2());
+            HighscorePrefs.setName2(HighscorePrefs.getName1());
+            HighscorePrefs.setCountry2(HighscorePrefs.getCountry1());
+            HighscorePrefs.setScore2(HighscorePrefs.getScore1());
+            setVisible();
+        } else if (correctAnswers >= HighscorePrefs.getScore2()) {
+            scorePosition = 2;
+            highEnough = true;
+            HighscorePrefs.setName3(HighscorePrefs.getName2());
+            HighscorePrefs.setCountry3(HighscorePrefs.getCountry2());
+            HighscorePrefs.setScore3(HighscorePrefs.getScore2());
+            setVisible();
+        } else if (correctAnswers >= HighscorePrefs.getScore3()) {
+            scorePosition = 3;
+            highEnough = true;
+            setVisible();
+        }
+    }
+
+    private void setVisible() {
+        textViewMadeIt.setVisibility(View.VISIBLE);
+        editTextName.setVisibility(View.VISIBLE);
+        saveScore.setVisibility(View.VISIBLE);
+        textViewMadeIt.setText("You made it to the " + scorePosition + ". place!");
+    }
+
+    public void saveToScores(View v) {
+        if (scorePosition == 1) {
+            HighscorePrefs.setName1(editTextName.getText().toString());
+            HighscorePrefs.setCountry1(GlobalPrefs.getCountry());
+            HighscorePrefs.setScore1(correctAnswers);
+        } else if (scorePosition == 2) {
+            HighscorePrefs.setName2(editTextName.getText().toString());
+            HighscorePrefs.setCountry2(GlobalPrefs.getCountry());
+            HighscorePrefs.setScore2(correctAnswers);
+        } else if (scorePosition == 3) {
+            HighscorePrefs.setName3(editTextName.getText().toString());
+            HighscorePrefs.setCountry3(GlobalPrefs.getCountry());
+            HighscorePrefs.setScore3(correctAnswers);
+        }
     }
 
     /**
